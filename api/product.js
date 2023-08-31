6a4e1390623662f2aa93a54a04dbbd84b9d8715a
chriistopher1 <142476784+chriistopher1@users.noreply.github.com>
2023-08-31 21:21:20 +0700

    Update product.js

--- api/product.js
+++ api/product.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Origin", "https://bliss-app-dun.vercel.app");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, Clickedbutton, Userid, Bookid");
   next();
